23fe59c4d6cc14999e04eccc2ef794730014826d
Chiranz <chiranpoudyal@gmail.com>
2019-01-04 11:47:08 +0530

    Home and Login Routes

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default LoginPage;
