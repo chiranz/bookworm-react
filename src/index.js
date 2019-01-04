f6928ff40e5099b0559cddccebf5fd2523539e33
Chiranz <chiranpoudyal@gmail.com>
2019-01-04 18:26:05 +0530

    React Redux setup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {
+  compositeWithDevTools,
+  composeWithDevTools
+} from "redux-devtools-extension";
+import RootReducer from "./RootReducer";
 import App from "./App";
-
 import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(
+  RootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 
   document.getElementById("root")
